06ba8ea2f038a722c922ef4e76c4723097515345
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2018-09-19 11:49:13 -0700

    Agregamos un usuario de control

--- GraficadorSeñales/MainWindow.xaml.cs
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ namespace GraficadorSeñales
 
         private void btnGraficar_Click(object sender, RoutedEventArgs e)
         {
-            double amplitud = double.Parse(txtAmplitud.Text);
-            double fase = double.Parse(txtFase.Text);
-            double frecuencia = double.Parse(txtFrecuencia.Text);
+           
             double tiempoInicial = double.Parse(txtTiempoInicial.Text);
             double tiempoFinal = double.Parse(txtTiempoFinal.Text);
             double frecuenciaMuestreo = double.Parse(txtFrecuenciaMuestreo.Text);
@@ -26,7 +24,13 @@ namespace GraficadorSeñales
             switch (cbTipoSeñal.SelectedIndex)
             {
                 //Señal Senoidal
-                case 0: señal = new SeñalSenoidal(amplitud, fase, frecuencia); //constructor
+                case 0:
+                    /*double amplitud = double.Parse(txtAmplitud.Text);
+                    double fase = double.Parse(txtFase.Text);
+                    double frecuencia = double.Parse(txtFrecuencia.Text);*/
+
+                    señal = new SeñalSenoidal(5, 0, 8); //constructor
+
                     break;
 
                 //Rampa
@@ -95,12 +99,35 @@ namespace GraficadorSeñales
 
             foreach (Muestra muestra in rampa.Muestras)
             {
-                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
+                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * 
+                    ((scrContenedor.Height / 2.0) - 30) * -1) + (scrContenedor.Height / 2)));
 
             }
            
         }
-        
+
+        private void cbTipoSeñal_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (panelConfiguracion != null)
+            {
+                panelConfiguracion.Children.Clear();
+
+                switch (cbTipoSeñal.SelectedIndex)
+                {
+                    case 0:  //Senoidal
+                        panelConfiguracion.Children.Add(new ConfiguracionSeñalSenoidal());
+                        break;
+
+                    case 1:
+
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+           
+        }
     }
 
 }
